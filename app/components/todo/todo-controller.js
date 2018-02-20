1f44834d9a28c7f5601d349ec33d02271dc83aba
Joe Davis <joecrush@msn.com>
2018-02-20 01:10:17 -0700

    just need to get delete working

--- app/components/todo/todo-controller.js
+++ app/components/todo/todo-controller.js
@@ -10,52 +10,40 @@ function TodoController() {
 	var todoListElem = document.getElementById('todoList')
 
 	// Use this getTodos function as your callback for all other edits
-	function getTodos(){
-		//FYI DONT EDIT ME :)
+	function getTodos() {
 		todoService.getTodos(drawTodos)
 	}
 
 	function drawTodos(todoList) {
-		//WHAT IS MY PURPOSE?
-		//BUILD YOUR TODO TEMPLATE HERE
-		
-		var todoTemplate = ''
+		var todoTemplate = `<p> Items To Do: ${todoList.length}</p>`
 		todoList.forEach(todo => {
 			todoTemplate += `
 			<div class="form-check">
 			<input class="form-check-input" type="checkbox" value="" id="todo">
 			<label class="form-check-label" for="todo">${todo.todo}</label>
-			<i onclick="app.controllers.todoCtrl.removeTodo('${todo.id}')" class="action remove fa fa-fw fa-sm fa-trash text-red"></i>
-			<p> Items To Do: ${todoList.length}</p>
+			<i onclick="app.controllers.todoCtrl.removeTodo('${todo.id}')" class="action remove fa fa-fw fa-md fa-trash text-red"></i>
 			</div>	
-			`				
+			`
 		})
 		todoListElem.innerHTML = todoTemplate
 	}
 
-	this.addTodo = function (e) {
-		e.preventDefault()
-		debugger
-		var form = e.target
-		console.log(form, "did I get here?")
+	this.addTodo = function (event) {
+		event.preventDefault()
+		var form = event.target
+		console.log(form)
 		todoService.addTodo(form, drawTodos)
 		form.reset()
-		                         //^^^^^^^ EXAMPLE OF HOW TO GET YOUR TOODOS AFTER AN EDIT
 	}
 
 	this.toggleTodoStatus = function (todoId) {
-		// asks the service to edit the todo status
 		todoService.toggleTodoStatus(todoId, getTodos)
-		// YEP THATS IT FOR ME
 	}
 
 	this.removeTodo = function (todoId) {
-		// ask the service to run the remove todo with this id
+		debugger
 		todoService.removeTodo(todoId, getTodos)
-		// ^^^^ THIS LINE OF CODE PROBABLY LOOKS VERY SIMILAR TO THE toggleTodoStatus
 	}
 
 	getTodos()
-	// IF YOU WANT YOUR TODO LIST TO DRAW WHEN THE PAGE FIRST LOADS WHAT SHOULD YOU CALL HERE???
-
 }
--- app/components/todo/todo-service.js
+++ app/components/todo/todo-service.js
@@ -1,67 +1,76 @@
 function TodoService() {
-	// A local copy of your todos
-	var todoList = []
 	var baseUrl = 'https://inspire-server.herokuapp.com/api/joedavis'
+	var todoList = []
 
-	function logError(err) {
-		console.error('UMM SOMETHING BROKE: ', err)
-		//CAN YOU NOTIFY THE USER IF SOMETHING BREAKS? 
-		//do this without breaking the controller/service responsibilities
+	function Todo(formData) {
+		this.todo = formData.todo.value;
 	}
 
-	function Todo(form){
-		this.todo = formula.todo.value;
-	}
-
-	this.getTodos = function (callBack) {
-		$.get(baseUrl)
-			.then(function (res) { 
-				todoList = res
-				callBack(todoList)
+	this.getTodos = function getTodos(cb) {
+		$.get(baseUrl + 'todos')
+			.then(function (todos) {
+				todoList =  todos
+				cb(todoList)
 			})
-			.fail(logError)
 	}
 
-	this.addTodo = function addTodo(form, cb) {
-		var todo  = new Todo(form)
-		$.post(baseUrl, todo)
-			.then(function(res){ // <-- WHAT DO YOU DO AFTER CREATING A NEW TODO?
+	this.addTodo = function (formData,cb) {
+		var todo = new Todo(formData)
+		$.post(baseUrl + 'todos', todo)
+			.then(res => {
 				todoList.unshift(res.data)
-				cb(todo)
-			}) 
-			.fail(logError)
+				cb(todoList)
+			})
 	}
 
-	this.toggleTodoStatus = function toggleTodoStatus (todoId, callBack) {
-		// MAKE SURE WE THINK THIS ONE THROUGH
-		//STEP 1: Find the todo by its index **HINT** todoList
-
-		//STEP 2: Change the completed flag to the opposite of what is is **HINT** todo.completed = !todo.completed
-
-		//STEP 3: Here is that weird Ajax request because $.put doesn't exist
+	this.toggleTodoSatus = function toggleTodoStatus(todoId, cb){
 		$.ajax({
 			method: 'PUT',
 			contentType: 'application/json',
 			url: baseUrl + '/' + todoId,
-			data: JSON.stringify(todoList),
+			data: JSON.stringify(todoList)
 		})
 			.then(function (res) {
-				this.getTodos(callBack)
+				this.getTodos(cb)
 			})
-			.fail(logError)
 	}
 
-	this.removeTodo = function (todoId, callBack) {
+	this.removeTodo = function removeTodo(todoId, cb) {
 		$.ajax({
+			method: 'DELETE',
+			contentType: 'application/json',
 			url: baseUrl + '/' + todoId,
-			method: 'DELETE'
+			data: JSON.stringify(todoList)
 		})
-			.then(res => {
-				this.getTodos(callBack)
+			.then(function (res) {
+				this.getTodos(cb)
 		})
-		.fail(logError)
+
 
 		
 	}
 
 }
+
+
+
+
+	// this.toggleTodoStatus = function toggleTodoStatus (todoId, cb) {
+	// 	// MAKE SURE WE THINK THIS ONE THROUGH
+	// 	//STEP 1: Find the todo by its index **HINT** todoList
+
+	// 	//STEP 2: Change the completed flag to the opposite of what is is **HINT** todo.completed = !todo.completed
+
+	// 	//STEP 3: Here is that weird Ajax request because $.put doesn't exist
+	// 	$.ajax({
+	// 		method: 'PUT',
+	// 		contentType: 'application/json',
+	// 		url: baseUrl + '/' + todoId,
+	// 		data: JSON.stringify(todoList),
+	// 	})
+	// 		.then(function (res) {
+	// 			this.getTodos(cb)
+	// 		})
+	// 		.fail(logError)
+	// }
+
